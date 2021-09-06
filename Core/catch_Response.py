759d92079ac4f995bcb470ae6ae69099a4f9ed9c
ml0042993 <zhoujianxiong.1988@gmail.com>
2021-09-07 00:01:03 +0800

    获取网页一共多少页，并解决requests获取网页源代码乱码问题

--- Core/catch_Response.py
+++ Core/catch_Response.py
@@ -7,7 +7,12 @@ class Catch_Page:
 
 	def base_Catch(self):
 		try:
-			self.process_html=requests.get(self.base_url,headers=static.HEARD,timeout=1000)
+			self.process_html=requests.get(self.base_url,headers=static.HEARD,timeout=10)
+
+# encoding是从http中的header中的charset字段中提取的编码方式，若header中没有charset字段则默认为ISO-8859-1编码模式，则无法解析中文，这是乱码的原因。
+# apparent_encoding会从网页的内容中分析网页编码的方式，所以apparent_encoding比encoding更加准确。当网页出现乱码时可以把apparent_encoding的编码格式赋值给encoding。
+
+			self.process_html.encoding = self.process_html.apparent_encoding#解决乱码问题
 			return self.process_html#返回首页的html源代码
 		except(ConnectionError):
 			print("连接超时")
@@ -18,3 +23,4 @@ if __name__ == '__main__':
 	fun = Catch_Page(static.BASE_URL)
 	aaa = fun.base_Catch()
 	print(aaa.text)
+	# print(aaa.content)
--- Core/catch_Top_page.py
+++ Core/catch_Top_page.py
@@ -1,14 +1,19 @@
 from Core.catch_Response import Catch_Page
-from bs4 import BeautifulSoup
+from pyquery import PyQuery as pq
 import static
+
+
 class process_Html:
 	def __init__(self,base_Url):
 		self.url = base_Url
 		self.source_html = Catch_Page(self.url).base_Catch()#初始化得到的网页源文件代码
+		self.init_doc = pq(self.source_html.text)#将源文件初始化为PyQuery对象
 
-	def catch_Top_page(self):
-		print(self.source_html)
+	def catch_Total_page(self):#获取网页一共多少页
+		total_page = self.init_doc('.pageinfo strong:first-child')
+		#print(total_page.text())
+		return total_page
 
 if __name__ == '__main__':
 	fun = process_Html(static.BASE_URL)
-	fun.catch_Top_page()
+	fun.catch_Total_page()
