a69e28506c8e7b1c3a6485ff56ef4a25915920cf
ml0042993 <zhoujianxiong.1988@gmail.com>
2021-09-10 18:26:22 +0800

    完成二级遍历，完成整体下载，但存在下载二级页面不能正常下载的问题

--- Core/catch_Response.py
+++ Core/catch_Response.py
@@ -27,6 +27,20 @@ class Catch_Page:
 		'''
 		self.init_doc = pq(self.process_html.text)
 
+	def Catch_info(self,url):
+		try:
+			self.process_html = requests.get(url, headers=Site.HEARD.value, timeout=10)
+
+			# encoding是从http中的header中的charset字段中提取的编码方式，若header中没有charset字段则默认为ISO-8859-1编码模式，则无法解析中文，这是乱码的原因。
+			# apparent_encoding会从网页的内容中分析网页编码的方式，所以apparent_encoding比encoding更加准确。当网页出现乱码时可以把apparent_encoding的编码格式赋值给encoding。
+
+			self.process_html.encoding = self.process_html.apparent_encoding  # 解决乱码问题
+			return self.process_html  # 返回首页的html源代码
+		except(ConnectionError):
+			print("连接超时")
+		except Exception as e:
+			print("未知错误")
+			print(e.args)
 if __name__ == '__main__':
 	fun = Catch_Page(Site.BASE_URL.value)
 	aaa = fun.base_Catch()
--- Core/catch_Third_page.py
+++ Core/catch_Third_page.py
@@ -13,32 +13,49 @@ class catch_Second_page(Catch_Page):
 	def read_All_url(self):
 		url_path = "../TEMP/second_Url"
 		with open(url_path,"r",encoding="utf-8") as f:
-			itmes = f.readlines()
+			# itmes = f.readlines()
+			tap = 1
 			# self.mess = f.readlines(1)[0].replace('\n','')
-			# for itme in itmes:
-			# 	self.itmes_info = itme.replace("\n","")
-			# 	super().__init__(base_url=self.itmes_info)
-			# 	self.base_Catch()
-			# 	self.change_PyQuery()
-			# 	self.__total_page = self.init_doc  # 将源文件初始化为PyQuery对象
-			# 	self.joint_All_page()
-
-			self.itmes_info = "http://www.gllmh.com/ymgj/8060.html"
-			print(self.itmes_info)
-			super().__init__(base_url=self.itmes_info)
-			self.base_Catch()
-			self.change_PyQuery()
-			self.__total_page = self.init_doc  # 将源文件初始化为PyQuery对象
-			self.joint_All_page()
+			for itme in f.readlines():
+				print(itme)
+				print('开始第{}话'.format(str(tap)))
+				# self.itmes_info = itme.replace("\n","")
+				url = itme.replace("\n","")
+				self.Catch_info(url)
+				self.change_PyQuery()
+				self.__total_page = self.init_doc  # 将源文件初始化为PyQuery对象
+				self.joint_All_page()
+				print('结束第{}话'.format(str(tap)))
+				tap+=1
+			# self.itmes_info = "http://www.gllmh.com/ymgj/8060.html"
+			# print(self.itmes_info)
+			# super().__init__(base_url=self.itmes_info)
+			# self.base_Catch()
+			# self.change_PyQuery()
+			# self.__total_page = self.init_doc  # 将源文件初始化为PyQuery对象
+			# self.joint_All_page()
 
 
 	def joint_All_page(self):
-		file_name = self.__total_page('h3').text()
+		file_name = self.__total_page('.listltitle h3').text().replace(" ","")
 		path_name = Site.DownLoad_Path.value + '/' +file_name
 		if not os.path.exists(path_name):
 			os.mkdir(path_name)
-		print(path_name)
-		#
+		# print(path_name)
+		img_DL=self.__total_page('section p img').items()
+		with open(path_name+'/{}'.format(file_name),'w',encoding="utf-8") as f:
+			tap = 1
+			for item in img_DL:
+
+				item = item.attr("src")
+				f.write(item + "\n")
+				super().__init__(item)
+				result = self.base_Catch()
+				print(result)
+				with open(path_name+'/{}'.format(str(tap)+'.jpg'),'wb') as ff:
+					ff.write(result.content)
+				tap+=1
+				# print(item)
 		# with open("../TEMP/second_Url", "a", encoding="utf-8") as f:
 		# 	for item in items:
 		# 		item = item.attr('href')#<class 'pyquery.pyquery.PyQuery'>
